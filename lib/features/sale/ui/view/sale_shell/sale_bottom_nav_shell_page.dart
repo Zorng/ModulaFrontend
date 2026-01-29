ba6091f64546b097d7da09d2852d5663e1f98e8e
alyceeIn_herCodeLand <nikachhun@Nikas-MacBook-Air.local>
2026-01-29 10:00:39 +0700

    fix sale navigation

--- lib/features/sale/ui/view/sale_shell/sale_bottom_nav_shell_page.dart
+++ lib/features/sale/ui/view/sale_shell/sale_bottom_nav_shell_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:modular_pos/core/routing/app_router.dart';
+import 'package:modular_pos/core/theme/responsive.dart';
 import 'package:modular_pos/core/widgets/navigation/app_bottom_nav_shell_scaffold.dart';
+import 'package:modular_pos/features/sale/ui/view/sale_cart/widgets/sale_cart_panel.dart';
 
 class SaleBottomNavShellPage extends StatelessWidget {
   const SaleBottomNavShellPage({
@@ -35,34 +37,85 @@ class SaleBottomNavShellPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final index = navigationShell.currentIndex;
+    final isWide = AppBreakpoints.isLarge(
+      MediaQuery.of(context).size.width,
+    );
+
+    if (isWide) {
+      if (index == 1) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          navigationShell.goBranch(0);
+        });
+      }
+
+      final wideItems = <BottomNavigationBarItem>[
+        _items[0],
+        _items[2],
+      ];
+      final wideIndex = index == 2 ? 1 : 0;
+
+      final showCartPanel = index == 0;
+      final appBarTitle = index == 2 ? _titles[2] : _titles[0];
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(appBarTitle),
+          centerTitle: false,
+          actions: actionsForIndex(index, context),
+        ),
+        body: Row(
+          children: [
+            Expanded(child: navigationShell),
+            if (showCartPanel) ...[
+              const VerticalDivider(width: 1),
+              const SizedBox(
+                width: 360,
+                child: SaleCartPanel(
+                  contentPadding: EdgeInsets.all(16),
+                ),
+              ),
+            ],
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: wideIndex,
+          items: wideItems,
+          type: BottomNavigationBarType.fixed,
+          onTap: (tabIndex) {
+            navigationShell.goBranch(tabIndex == 0 ? 0 : 2);
+          },
+        ),
+      );
+    }
 
     return AppBottomNavShellScaffold(
       navigationShell: navigationShell,
       titles: _titles,
       items: _items,
       centerTitle: false,
-      actions:
-          index == 1
-              ? [
-                PopupMenuButton<String>(
-                  onSelected: (value) {
-                    if (value == 'view_carts') {
-                      context.push(AppRoute.saleViewCarts.path);
-                    }
-                  },
-                  itemBuilder:
-                      (_) => const [
-                        PopupMenuItem<String>(
-                          value: 'view_carts',
-                          child: Text('View carts'),
-                        ),
-                      ],
-                ),
-              ]
-              : null,
+      actions: actionsForIndex(index, context),
       onBackPressed: () => context.go(AppRoute.portal.path),
       backIcon: Icons.home_outlined,
       backTooltip: 'Home',
     );
+  }
+
+  List<Widget>? actionsForIndex(int index, BuildContext context) {
+    if (index != 1) return null;
+    return [
+      PopupMenuButton<String>(
+        onSelected: (value) {
+          if (value == 'view_carts') {
+            context.push(AppRoute.saleViewCarts.path);
+          }
+        },
+        itemBuilder:
+            (_) => const [
+              PopupMenuItem<String>(
+                value: 'view_carts',
+                child: Text('View carts'),
+              ),
+            ],
+      ),
+    ];
   }
 }
