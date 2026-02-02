3683ff1a5e5f37114613fc56203305839d8f83af
alyceeIn_herCodeLand <nikachhun@192.168.1.6>
2026-02-02 21:15:20 +0700

    implement bottom navigation for cash session

--- lib/core/routing/routes/cash_routes.dart
+++ lib/core/routing/routes/cash_routes.dart
@@ -1,20 +1,48 @@
 import 'package:go_router/go_router.dart';
 import 'package:modular_pos/core/routing/app_router.dart';
 import 'package:modular_pos/features/cash_session/ui/view/cashier_cash_session.dart';
+import 'package:modular_pos/features/cash_session/ui/view/cash_movement/cash_movement_page.dart';
+import 'package:modular_pos/features/cash_session/ui/view/cash_shell/cash_bottom_nav_shell_page.dart';
 import 'package:modular_pos/features/cash_session/ui/view/x_report/x_report_page.dart';
 import 'package:modular_pos/features/cash_session/ui/view/z_report/z_report_page.dart';
 
 List<RouteBase> buildCashRoutes() {
   return [
-    GoRoute(
-      path: AppRoute.cashSession.path,
-      name: AppRoute.cashSession.name,
-      builder: (context, state) => const CashSessionScreen(),
-    ),
-    GoRoute(
-      path: AppRoute.xReport.path,
-      name: AppRoute.xReport.name,
-      builder: (context, state) => const XReportPage(),
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return CashBottomNavShellPage(navigationShell: navigationShell);
+      },
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: AppRoute.cashSession.path,
+              name: AppRoute.cashSession.name,
+              builder: (context, state) =>
+                  const CashSessionScreen(showAppBar: false),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: AppRoute.cashMovement.path,
+              name: AppRoute.cashMovement.name,
+              builder: (context, state) => const CashMovementPage(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: AppRoute.xReport.path,
+              name: AppRoute.xReport.name,
+              builder: (context, state) =>
+                  const XReportPage(showAppBar: false),
+            ),
+          ],
+        ),
+      ],
     ),
     GoRoute(
       path: AppRoute.zReport.path,
